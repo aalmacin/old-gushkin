db67d434bbd7e0b9bba0276d4103d695a11ebf03
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2020-02-26 08:38:03 -0500

    Added working api query

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 import classes from './Main.module.scss';
+import axios from 'axios';
+import { useCookies } from 'react-cookie';
 
 function Main() {
+  const [cookies] = useCookies(['gushkinTokens']);
+
+  const url: any = process.env.REACT_APP_APP_SYNC_URL;
+  axios.post(url, {
+    query: `
+      query WishItems {
+        getWishItemsForUser(userId: "1") {
+          id
+          status
+        }
+      }
+    `
+  }, {
+    headers: {
+      authorization: cookies.gushkinTokens.accessToken
+    }
+  }).then(r => {
+    console.log(r);
+  }).catch(err => {
+    console.log(err);
+  })
+
   return (
     <div className={classes.Main}>
       Main
